a51e9f5dc65040d842570a3a87a8feb2d03702bd
jayeebee <pnuthexsa311@gmail.com>
2021-03-07 10:25:16 -0500

    possible final form

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,156 @@
 // Assignment code here
-function writePassword(){
-  var passwordLength = window.prompt('How long should your password be?');
-  console.log (passwordLength);
-  if (passwordLength < 8 || passwordLength > 128){     
-    window.alert ('Please enter a value from 8-128');
-    return writePassword();
-  } 
 
-  var upperCaseConfirm = window.confirm('Does your password require Upper Case letters?');
-  console.log (upperCaseConfirm);
-  var numberConfirm = window.confirm('Does your password require numbers?');
-  console.log (numberConfirm);
-  var symbolConfirm = window.confirm('Does your password require symbols?');
-  console.log (symbolConfirm);
+function generatePassword() {
+  var passwordLength = window.prompt("How long should your password be?");
+  while (passwordLength < 8 || passwordLength > 128)
+    passwordLength = window.prompt("Please enter a value from 8-128");
+
+  var lowerCaseConfirm = window.confirm(
+    "Does your password require Lower Case Letters?"
+  );
+
+  var upperCaseConfirm = window.confirm(
+    "Does your password require Upper Case letters?"
+  );
+
+  var numberConfirm = window.confirm("Does your password require numbers?");
+
+  var symbolConfirm = window.confirm("Does your password require symbols?");
+
+  if (
+    lowerCaseConfirm === false &&
+    upperCaseConfirm === false &&
+    numberConfirm === false &&
+    symbolConfirm === false
+  ) {
+        window.alert(
+      "Choose at least one type of character set for your password!!!"
+    );
+    return generatePassword();
+  }
+ 
+
+  var passwordCharacters = []
+    
+  if (symbolConfirm) {
+    passwordCharacters = passwordCharacters.concat(symbols)
+  }
+
+  if (numberConfirm) {
+    passwordCharacters = passwordCharacters.concat(numbers)
+  }
+    
+  if (lowerCaseConfirm) {
+    passwordCharacters = passwordCharacters.concat(lowerCharacters)
+  }
+
+  if (upperCaseConfirm) {
+    passwordCharacters = passwordCharacters.concat(upperCharacters)
+  }
+
+  console.log(passwordCharacters)
+
+  var password = [];
+
+  for (var i = 0; i < passwordLength; i++) {
+    password = password + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+    console.log(password)};
+
+
+
+var passwordText = document.querySelector("#password");
+passwordText.value = password;
 }
 
 
+// Write password to the #password input
+function writePassword() {
+var password = generatePassword();
+}
 
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-/*function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+generateBtn.addEventListener("click", generatePassword);
 
-  passwordText.value = password;
+var lowerCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
 
-}*/
+var upperCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+var numbers = [
+  "0",
+  "1",
+  "2",
+  "3", 
+  "4", 
+  "5", 
+  "6", 
+  "7", 
+  "8", 
+  "9"
+];
 
-
-var lowerCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-var upperCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-var symbols = ['!', '?', '@', '#', '$', '%', '^', '&', '*'];
-
-
-var randomUpper = upperCharacters[Math.floor(Math.random()*upperCharacters.length)];
-var randomLower = lowerCharacters[Math.floor(Math.random()*lowerCharacters.length)];
-var randomNumber = numbers[Math.floor(Math.random()*numbers.length)];
-var randomSymbol = symbols[Math.floor(Math.random()*symbols.length)];
-
-console.log (randomLower)
-console.log (randomUpper)
-console.log (randomNumber)
-console.log (randomSymbol)
+var symbols = [
+  "!",
+  "?", 
+  "@", 
+  "#", 
+  "$", 
+  "%", 
+  "^", 
+  "&", 
+  "*"
+]
